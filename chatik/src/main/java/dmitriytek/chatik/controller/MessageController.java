97a15738a7b81b2c3f82edfcc571678f6622eb7d
Dmitriy <dmitriytek@gmail.com>
2019-02-20 17:10:43 +0300

    REST: add websocket MainControllet & WebSocketConfig; Vue: add SockJS & Stomp, refactor saving message (chatik)

--- chatik/src/main/java/dmitriytek/chatik/controller/MessageController.java
+++ chatik/src/main/java/dmitriytek/chatik/controller/MessageController.java
@@ -5,6 +5,8 @@ import dmitriytek.chatik.config.Views;
 import dmitriytek.chatik.domain.Message;
 import dmitriytek.chatik.repo.MessageRepo;
 import org.springframework.beans.BeanUtils;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -48,5 +50,12 @@ public class MessageController {
     @DeleteMapping("{id}")
     public void deleteMessage(@PathVariable("id") Message message){
         messageRepo.delete(message);
+    }
+
+    //websocket
+    @MessageMapping("/changeMessage")
+    @SendTo("/topic/activity")
+    public Message change(Message message){
+        return messageRepo.save(message);
     }
 }
--- chatik/src/main/resources/static/js/pages/App.vue
+++ chatik/src/main/resources/static/js/pages/App.vue
@@ -4,6 +4,9 @@
 
 <script>
     import MessagesList from 'components/messages/MessageList.vue'
+    import { addHandler } from "util/ws";
+    import { getIndex } from "util/collections";
+
     export default {
         components: {
             MessagesList
@@ -12,6 +15,16 @@
             return{
                 messages: frontendData.messages
             }
+        },
+        created() {
+            addHandler(data =>{
+                let index = getIndex(this.messages, data.id)
+                if(index > -1){
+                    this.messages.splice(index, 1, data)
+                } else {
+                    this.messages.push(data)
+                }
+            })
         }
     }
 </script>
